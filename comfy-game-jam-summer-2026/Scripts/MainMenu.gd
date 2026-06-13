978ee7a554f71cfc1fd9b42d50f237559895b9d1
isc-bll <isacan0527@gmail.com>
2026-06-12 17:07:39 -0700

    added code for hover effect to buttons

--- comfy-game-jam-summer-2026/Scripts/MainMenu.gd
+++ comfy-game-jam-summer-2026/Scripts/MainMenu.gd
@@ -1,7 +1,18 @@
 extends Control
 
+func _ready() -> void:
+	for button in $ButtonsContainer.get_children():
+		button.mouse_entered.connect(enter_hover.bind(button))
+		button.mouse_exited.connect(exit_hover.bind(button))
+
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level1/level1.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func enter_hover(button) -> void:
+	pass # optional hover effect
+	
+func exit_hover(button) -> void:
+	pass # optional hover effect
