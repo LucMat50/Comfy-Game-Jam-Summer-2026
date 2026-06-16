5aceccbf99e07864f286565e38526d4036debb49
MLJoson <littlemjoson@gmail.com>
2026-06-16 01:11:59 -0700

    Added tree prefab that clips to ground + almost finished first map

---
+++ comfy-game-jam-summer-2026/Scripts/dropThings.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+#snap every "thing" to the ground
+func _ready():
+	print("Forest script running")
+	for child in get_children():
+		print(child.name, " ", child.get_class())
+	
+	for child in get_children():
+		if child is Node3D:
+			snap_to_ground(child)
+
+#spawn the tree in the sky and raycast it to the ground
+func snap_to_ground(tree: Node3D):
+	var start = tree.global_position + Vector3.UP * 1000
+	var end = tree.global_position + Vector3.DOWN * 1000
+	var query = PhysicsRayQueryParameters3D.create(start, end)
+	var result = get_world_3d().direct_space_state.intersect_ray(query)
+	print("Tree:", tree.name)
+	print("Hit:", result)
+	if result:
+		tree.global_position = result.position
