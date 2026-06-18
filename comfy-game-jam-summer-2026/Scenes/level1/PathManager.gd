86ec3bf84db91cb7e54b9e8e239a798ed5b49c0e
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2026-06-18 10:53:47 -0700

    change from array implementation to reference based
    changes going through paths based on references rather than going through an array

--- comfy-game-jam-summer-2026/Scenes/level1/PathManager.gd
+++ comfy-game-jam-summer-2026/Scenes/level1/PathManager.gd
@@ -2,26 +2,26 @@ extends Node3D
 
 @export var player : Node3D
 var currentPath : Path3D
-var paths : Array[Path3D] = [] 
-var currentPathIndex = 0
+var leftPath : Path3D
+var rightPath : Path3D
 var pathTangent : Vector3
-var startingPathIndex = 0
-
+var leftIndex = 0
+var rightIndex = 1
 
 # stores all the paths in a single array
 func _ready() -> void:
 	print("Now populating path list")
 	populatePaths()
+	getPathChildren(currentPath)
+	return
 
 func _process(delta: float) -> void:
 	pathTangent = getPathTangent()
 	player.setForwadDirection(pathTangent)
+	return
 	
 func populatePaths() -> void:
-	for PathManager_child in get_children():
-		if PathManager_child is Path3D:
-			paths.append(PathManager_child)
-	currentPath = paths[startingPathIndex]
+	currentPath = get_child(0)
 	if currentPath == null:
 		push_error("Cannot find starting path: add a Curve2D as a child to PathManager")
 	else:
@@ -31,21 +31,23 @@ func getPathTangent() -> Vector3:
 	var curve = currentPath.curve
 	var offset = curve.get_closest_offset(player.position)
 	return -curve.sample_baked_with_rotation(offset, true).basis.z
-	
-	
-#Moves left +1 like binary tree
-func moveLeft() -> void:
-	currentPathIndex += 1
-	if currentPathIndex > paths.size():
-		push_error("No path exists in PathManager Node: add a Curve2D to path manager")
-	currentPath = paths[currentPathIndex]
 
-#Moves right +2 like binary tree
-func moveRight() -> void:
-	currentPathIndex += 2
-	if currentPathIndex > paths.size():
-		push_error("No path exists in PathManager Node: add a Curve2D to path manager")
-		currentPath = paths[currentPathIndex]
+#Left is always 0
+func getPathChildren(path: Path3D) -> void:
+	leftPath = path.get_child(leftIndex)
+	rightPath = path.get_child(rightIndex)
+
+func goLeft() -> void:
+	if leftPath == null:
+		push_error("Left path is null: Assign a Path3D tp child")
+	currentPath = leftPath
+	getPathChildren(currentPath)
+	
+func goRight() -> void:
+	if rightPath == null:
+		push_error("Left path is null: Assign a Path3D tp child")
+	currentPath = rightPath
+	getPathChildren(currentPath)
 	
 func choosePath() -> void:
 	print("Now Choosing...")
