22c6058507a39b9b24fc4bb9a5faa12bc088c3c9
LucMat50 <lmaturino5@gmail.com>
2026-06-18 23:23:29 -0700

    good ending animation

--- comfy-game-jam-summer-2026/Scripts/good_ending.gd
+++ comfy-game-jam-summer-2026/Scripts/good_ending.gd
@@ -5,5 +5,21 @@ extends Control
 @onready var background_animation = $Background/AnimationPlayer
 
 func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	character_animation.play("beach")
+	await get_tree().create_timer(3).timeout
+	Dialogic.start("trail3right")
+
+func _on_dialogic_signal(argument: String):
+	character_animation.play(argument)
+	
+	if argument == "there":
+		background_animation.play("chud")
+	elif argument == "here":
+		background_animation.play("chencho")
+	elif argument == "yur":
+		background_animation.play("chud")
+		
+	await Dialogic.timeline_ended
 	await get_tree().create_timer(2).timeout
+	character_animation.play("eating")
